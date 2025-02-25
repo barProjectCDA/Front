d0e872d1ccf562c5f5c10a799e2e05a0cee174f5
OmmarAmmar0 <omar.ammargs@gmail.com>
2025-02-25 16:20:49 +0100

    user

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,27 +1,44 @@
 import styles from "../assets/styles/Service.module.css";
 import { Link } from "react-router-dom";
- 
+
 const Services = () => {
-    return (
-        <>
-            <div className={styles.servicesContainer}>
-                <h2>Services</h2>
-            </div>
-            <div className={styles.buttonsContainer}>
-                <div>
-                    <button><Link to="/servicesproduit" className={styles.link}>Produit</Link></button>
-                    <button><Link to="" className={styles.link}>Table</Link></button>
-                    <button><Link to="" className={styles.link}>Statistique</Link></button>
-                </div>
-            </div>
-            <div className={styles.buttonsRetour}>
-                <div>
-                    <button><Link to="/" className={styles.link}>Retour</Link></button>
-                </div>
-            </div>
- 
-        </>
-    );
+  return (
+    <>
+      <section className={styles.sectionsContainer}>
+        <div className={styles.servicesContainer}>
+          <h2>Services</h2>
+        </div>
+        <div className={styles.buttonsContainer}>
+          <div>
+            <button>
+              <Link to="/servicesproduit" className={styles.link}>
+                Produit
+              </Link>
+            </button>
+            <button>
+              <Link to="" className={styles.link}>
+                Table
+              </Link>
+            </button>
+            <button>
+              <Link to="" className={styles.link}>
+                Statistique
+              </Link>
+            </button>
+          </div>
+        </div>
+        <div className={styles.buttonsRetour}>
+          <div>
+            <button>
+              <Link to="/" className={styles.link}>
+                Retour
+              </Link>
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
- 
+
 export default Services;
